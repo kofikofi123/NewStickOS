385fe0eb251d9ec8c5ee145e59551b2516881fdf
ChairTriangle <usingnamespaceasd@gmail.com>
2017-06-08 22:09:19 -0400

    Create compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-#since I couldnt cross compile my self because of size on c9 and koding, I an already cross compiler
+#OLD: since I couldnt cross compile my self because of size on c9 and koding, I am using an already cross compiler
 
 export NOSP="$PWD"
 cd ../barebones-toolchain
