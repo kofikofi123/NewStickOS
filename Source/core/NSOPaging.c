fb25cd6a8c8093a3b2eb87a63fb08d4c4aa2c18a
Kofi <usingnamespaceasd@gmail.com>
2019-02-19 00:07:12 -0500

    for later

--- Source/core/NSOPaging.c
+++ Source/core/NSOPaging.c
@@ -3,10 +3,9 @@
 #include "NSOBochs.h"
 
 void kernel_initPaging(){
-	kernel_printfBOCHS("cpuid enabled: %b", kernel_isCpuidSupported());
-
+	
 }
-/*
-u8 kernel_reservePage(void*);
+
+/*u8 kernel_reservePage(void*);
 u8 kernel_reservePage2(void*, void*);
 u8 kernel_releasePage(void*);*/
