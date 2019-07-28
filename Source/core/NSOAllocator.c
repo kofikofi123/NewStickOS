304115994d4a5238c01d1b5686ae47f0fb9654ae
Kofi <usingnamespaceasd@gmail.com>
2019-07-28 03:26:18 -0400

    git-save

--- Source/core/NSOAllocator.c
+++ Source/core/NSOAllocator.c
@@ -79,7 +79,8 @@ void* kernel_malloc(u32 size, u8 alignment){
 		if (oldNext != end)
 			oldNext->prev = node2;
 		oldPrev->next = node2;
-	}
+	}else
+		kernel_printfBOCHS("Malloc|Newnode: %x(%x)|oldnode: %x\n|prev: %x|next: %x|size:%x\n", (u32)node2, node2->size, (u32)node, (u32)oldPrev, (u32)oldNext, size);
 	return final;
 }
 
